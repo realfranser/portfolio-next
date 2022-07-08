d6b8e74c6013e21551840e8a3cff01bc8294645b
realfranser <serranoarresejavier@gmail.com>
2022-07-08 21:48:03 +0200

    Fixed issue with chrome image

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Head from "next/head";
 
 import blogEntries from "../../locales/es-ES/blog/blogEntries.json";
 import { BlogEntry, IBlogEntryData } from "../../components/BlogEntry";
@@ -7,13 +7,23 @@ const blogEntryDataList: IBlogEntryData[] = blogEntries;
 
 const blog = () => {
   return (
-    <div id="blog">
-      <h2 className="text-center">Mi blog de tecnología</h2>
-      <div className="text-center py-4 text-xl font-bold uppercase tracking-wider"></div>
-      {blogEntryDataList.map((item: IBlogEntryData, index) => (
-        <BlogEntry key={index} {...item} />
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Fran | Blog</title>
+        <meta
+          name="description"
+          content="I'm a Junior Software Engineer passionate about learning new technologies"
+        />
+        <link rel="icon" href="/assets/nextLogoFull.png" />
+      </Head>
+      <div id="blog">
+        <h2 className="text-center">Mi blog de tecnología</h2>
+        <div className="text-center py-4 text-xl font-bold uppercase tracking-wider"></div>
+        {blogEntryDataList.map((item: IBlogEntryData, index) => (
+          <BlogEntry key={index} {...item} />
+        ))}
+      </div>
+    </>
   );
 };
 
